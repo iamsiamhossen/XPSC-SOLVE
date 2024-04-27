58ca539559e2dcc9dadbad342a15024028adb9fa
iamsiamhossen <141014441+iamsiamhossen@users.noreply.github.com>
2024-04-27 23:25:20 +0600

    Add files via upload

---
+++ Week 6/Day 2/Factory_Machines.cpp
@@ -0,0 +1,103 @@
+/**
+ * Author: iamsiamhossen
+ * Created: 27-04-2024 20:01:52
+ **/
+#include <bits/stdc++.h>
+#define fastread() (ios_base::sync_with_stdio(0), cin.tie(0))
+#define endl "\n"
+// HighLoad
+#define ll long long int
+#define pb push_back
+#define ld long double
+#define pi pair<ll, ll>
+#define map map<int, int>
+#define setd set<int, greater<int>>
+#define seta set<int>
+// next three are handy ways to get ints, it's also force you to use '&' sign
+#define GET1(a) scanf("%d", &a)
+#define GET2(a, b) scanf("%d%d", &a, &b)
+#define GET3(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define GETS(x) scanf("%s", x) // get a char* string
+#define INIT1(n) \
+    LL(n);       \
+    scanf("%lld", &(n))
+#define INIT2(n, m) \
+    LL(n), (m);     \
+    scanf("%lld%lld", &(n), &(m))
+#define INIT3(n, m, k) \
+    LL(n), (m), (k);   \
+    scanf("%lld%lld%lld", &(n), &(m), &(k))
+// Vector
+#define vec vector<ll>;
+#define vvi vector<vi>;
+// Vector Pair
+#define vectorpair vector<ii>;
+#define vvii vector<vii>;
+// MOD
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define INF 1001001001
+// for map, pair
+#define mp make_pair
+#define fi first
+#define se second
+// directions
+const int fx[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+const int fxx[8][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+#define checkmate return 0;
+using namespace std;
+void solve()
+{
+    ll n, key;
+    cin >> n >> key;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    // Lamda Function 
+    auto ok = [&](long long mid)
+    {
+        long long t = 0;
+        for (int i = 0; i < n; i++)
+        {
+            t += (mid / a[i]);
+            if (t >= key)
+            {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    ll l = 1,
+       r = 1e18, mid, ans;
+
+    while (l <= r)
+    {
+        mid = (l + r) / 2;
+        if (ok(mid))
+        {
+            ans = mid;
+            r = mid - 1;
+        }
+        else
+        {
+            l = mid + 1;
+        }
+    }
+    cout << ans << endl;
+}
+int main()
+{
+    fastread();
+    // int t;
+    // cin >> t;
+    // while (t--)
+    // {
+    //     solve();
+    // }
+    solve();
+    checkmate;
+}

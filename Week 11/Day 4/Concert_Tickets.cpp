0bd0d29d9634830b539e309ea79a07ba1a98255c
iamsiamhossen <141014441+iamsiamhossen@users.noreply.github.com>
2024-06-04 20:39:50 +0600

    Add files via upload

---
+++ Week 11/Day 4/Concert_Tickets.cpp
@@ -0,0 +1,106 @@
+/**
+ * Author: iamsiamhossen
+ * Created: 04-06-2024 02:34:11
+ **/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define fastread() (ios_base::sync_with_stdio(0), cin.tie(0))
+#define endl "\n"
+// HighLoad
+#define ll long long int
+#define pb push_back
+#define ld long double
+#define pi pair<ll, ll>
+#define map map<int, int>
+#define setd set<int, greater<int>>
+#define seta set<int>
+// next three are handy ways to get ints, it's also force you to use '&' sign
+#define GET1(a) scanf("%d", &a)
+#define GET2(a, b) scanf("%d%d", &a, &b)
+#define GET3(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define GETS(x) scanf("%s", x) // get a char* string
+#define INIT1(n) \
+    LL(n);       \
+    scanf("%lld", &(n))
+#define INIT2(n, m) \
+    LL(n), (m);     \
+    scanf("%lld%lld", &(n), &(m))
+#define INIT3(n, m, k) \
+    LL(n), (m), (k);   \
+    scanf("%lld%lld%lld", &(n), &(m), &(k))
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define Yes cout << "Yes\n"
+#define No cout << "No\n"
+#define Ok cout << "Ok\n"
+// Vector
+#define vec vector<ll>;
+#define vvi vector<vi>;
+// Vector Pair
+#define vectorpair vector<ii>;
+#define vvii vector<vii>;
+// MOD
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define INF 1001001001
+// for map, pair
+#define mp make_pair
+#define fi first
+#define se second
+#define forn(i, n) for (ll i = 0; i < int(n); i++)
+#define all(x) (x).begin(), (x).end()
+// directions
+const int fx[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+const int fxx[8][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+#define checkmate return 0;
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    multiset<int> a;
+    vector<int> b(m);
+    forn(i, n)
+    {
+        int t;
+        cin >> t;
+        a.insert(t);
+    }
+    forn(i, m)
+    {
+        cin >> b[i];
+    }
+
+    forn(i, m)
+    {
+        auto it = a.upper_bound(b[i]);
+        if (it == a.begin())
+        {
+            cout << "-1" << endl;
+        }
+        else
+        {
+            it--;
+            cout << *it << endl;
+            a.erase(it);
+        }
+    }
+}
+int main()
+{
+    fastread();
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+        // cout << "Case " << t << ": ";
+        solve();
+    }
+    checkmate;
+}

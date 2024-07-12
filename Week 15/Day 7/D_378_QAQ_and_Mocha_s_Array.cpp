e2db1ab3c7051dfec2deb151dcadca11ae892294
iamsiamhossen <141014441+iamsiamhossen@users.noreply.github.com>
2024-07-12 23:41:16 +0600

    Add files via upload

---
+++ Week 15/Day 7/D_378_QAQ_and_Mocha_s_Array.cpp
@@ -0,0 +1,109 @@
+/**
+ * Author: iamsiamhossen
+ * Created: 12-07-2024 23:18:57
+ **/
+#include <bits/stdc++.h>
+#define fastread() (ios_base::sync_with_stdio(0), cin.tie(0))
+#define endl "\n"
+#define int long long
+#define float double
+// HighLoad
+#define ll long long int
+#define pb push_back
+#define ld long double
+#define pi pair<ll, ll>
+#define map map<int, int>
+#define setd set<int, greater<int>>
+#define seta set<int>
+// nect three are handy ways to get ints, it's also force you to use '&' sign
+#define GET1(a) scanf("%d", &a)
+#define GET2(a, b) scanf("%d%d", &a, &b)
+#define GET3(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define GETS(c) scanf("%s", c) // get a char* string
+#define INIT1(n) \
+    LL(n);       \
+    scanf("%lld", &(n))
+#define INIT2(n, m) \
+    LL(n), (m);     \
+    scanf("%lld%lld", &(n), &(m))
+#define INIT3(n, m, k) \
+    LL(n), (m), (k);   \
+    scanf("%lld%lld%lld", &(n), &(m), &(k))
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define Yes cout << "Yes\n"
+#define No cout << "No\n"
+#define Ok cout << "Ok\n"
+// Vector
+#define vec vector<ll>;
+#define vvi vector<vi>;
+// Vector Pair
+#define vectorpair vector<ii>;
+#define vvii vector<vii>;
+// MOD
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define INF 1001001001
+// for map, pair
+#define mp make_pair
+#define fi first
+#define se second
+#define forn (long long i = 0; i < long long(n); i++)
+#define all(c) (c).begin(), (c).end()
+// directions
+const int fc[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+const int fcc[8][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+#define checkmate return 0;
+using namespace std;
+// template <typename T>
+// using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+
+    int b = a[0], c, i = 0;
+    while (i < n)
+    {
+        if (a[i] % b != 0)
+        {
+            c = a[i];
+            break;
+        }
+        i++;
+    }
+    bool ok = true;
+    while (i < n)
+    {
+        if (a[i] % b != 0 and a[i] % c != 0)
+        {
+            ok = false;
+            break;
+                }
+        i++;
+    }
+    if (ok)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
+int32_t main()
+{
+    fastread();
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+        // cout << "Case " << t << ": ";
+        solve();
+    }
+    checkmate;
+}
